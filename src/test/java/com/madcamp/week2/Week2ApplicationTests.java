12dc6e412d465e548ee3aa4cfb88753d5b370b64
Jiwon, Cha <81519167+wona825@users.noreply.github.com>
2024-01-06 01:15:29 +0900

    Update Week2ApplicationTests.java

--- src/test/java/com/madcamp/week2/Week2ApplicationTests.java
+++ src/test/java/com/madcamp/week2/Week2ApplicationTests.java
@@ -3,7 +3,7 @@ package com.madcamp.week2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class Week2ApplicationTests {
 
     @Test
