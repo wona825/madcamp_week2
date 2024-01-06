b4de606d3f2a836942dce216185797960e57c063
chajiwon <wona825@naver.com>
2024-01-06 21:36:29 +0900

    fix: 로그인 시 반환되는 정보 수정

--- src/main/java/com/madcamp/week2/global/auth/social/SocialAuthenticationResponse.java
+++ src/main/java/com/madcamp/week2/global/auth/social/SocialAuthenticationResponse.java
@@ -4,15 +4,18 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
+
 @Data
 @Builder
 public class SocialAuthenticationResponse {
     @JsonProperty("access_token")
     private String accessToken;
 
-    @JsonProperty("refresh_token")
-    private String refreshToken;
-
     @JsonProperty("is_registered")
     private Boolean isRegistered;
+
+    private String nickname;
+
+    @JsonProperty("profile_img")
+    private String profileImg;
 }
