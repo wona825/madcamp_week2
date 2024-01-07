88b368c60ca3e496e8907d2fd42f1f72f1e1101f
chajiwon <wona825@naver.com>
2024-01-07 17:36:30 +0900

    fix: 사용자 프로필 이미지 삭제 에러 수정

--- src/main/java/com/madcamp/week2/domain/user/controller/UserController.java
+++ src/main/java/com/madcamp/week2/domain/user/controller/UserController.java
@@ -1,14 +1,12 @@
 package com.madcamp.week2.domain.user.controller;
 
-import com.madcamp.week2.domain.user.dto.ModifyUserRequest;
+import com.madcamp.week2.domain.user.dto.ModifyUser;
 import com.madcamp.week2.domain.user.entity.User;
 import com.madcamp.week2.domain.user.service.UserService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
@@ -19,9 +17,9 @@ public class UserController {
 
     private final UserService userService;
     @PostMapping("/modify/v1")
-    public ResponseEntity<?> modifyUser(@AuthenticationPrincipal User user, @RequestParam("img") MultipartFile multipartFile, String nickname) throws IOException {
-        userService.modifyUser(user, multipartFile, nickname);
+    public ResponseEntity<?> modifyUser(@AuthenticationPrincipal User user, @ModelAttribute ModifyUser.Request requestBody) throws IOException {
 
-        return ResponseEntity.ok().build();
+        ModifyUser.Response modifyUser = userService.modifyUser(user, requestBody);
+        return ResponseEntity.ok(modifyUser);
     }
 }
